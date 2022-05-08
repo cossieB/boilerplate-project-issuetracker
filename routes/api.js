e8a6a8aab7cd2e8f148b33c65f63712dc557490d
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-05-09 01:22:03 +0200

    FIN

--- routes/api.js
+++ routes/api.js
@@ -1,67 +1,114 @@
 'use strict';
 const Issue = require('../model')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const res = require('express/lib/response');
 
 module.exports = function (app) {
 
-    app.route('/api/issues/:project')
-        .get(async function (req, res) {
-            await mongoose.connect(process.env.MONGO_URI)
-            const fields = ['_id', 'issue_title', 'issue_text', 'created_by', 'assigned_to', 'status_text', 'created_on', 'updated_on', 'open' ]
-            let project = req.params.project;
-            let query = {}
-            for (const prop in req.query) {
-                if (fields.includes(prop.toLowerCase())) query[prop] = req.query[prop]
-            }
-            const result = await Issue.find(query)
-            res.json([...result])
-        })
+    try {
+        app.route('/api/issues/:project')
 
-        .post(async function (req, res) {
-            await mongoose.connect(process.env.MONGO_URI)
-            let project = req.params.project;
-            const { issue_title, issue_text, created_by, assigned_to, status_text } = req.body
-            const issue = new Issue({ ...req.body, open: true, created_on: new Date().toISOString(), updated_on: new Date().toISOString() })
-            await issue.save()
+            .get(async function (req, res) {
+                try {
 
-            res.json({ assigned_to, status_text, open: true, _id: issue.id, issue_title, issue_text, created_by, created_on: issue.created_on, updated_on: issue.updated_on })
+                    await mongoose.connect(process.env.MONGO_URI)
+                    let project = req.params.project;
+                    const fields = ['_id', 'issue_title', 'issue_text', 'created_by', 'assigned_to', 'status_text', 'created_on', 'updated_on', 'open']
+                    let query = {project}
+                    for (const prop in req.query) {
+                        if (fields.includes(prop.toLowerCase())) query[prop] = req.query[prop]
+                    }
+                    const result = await Issue.find(query).select('-project')
+                    res.json([...result])
+                }
+                finally {
+                    mongoose.connection.close()
+                }
+            })
 
-        })
+            .post(async function (req, res) {
+                try {
 
-        .put(async function (req, res) {
-            await mongoose.connect(process.env.MONGO_URI)
-            let project = req.params.project;
+                    await mongoose.connect(process.env.MONGO_URI)
+                    let project = req.params.project;
+                    const { issue_title, issue_text, created_by } = req.body;
+                    const assigned_to = req.body.assigned_to || ""
+                    const status_text = req.body.status_text || ""
+                    const issue = new Issue({ ...req.body, project, open: true, created_on: new Date().toISOString(), updated_on: new Date().toISOString() })
+                    console.log(status_text)
+                    try {
+                        await issue.save()
+                    }
+                    catch (e) {
+                        return res.status(200).json( { error: 'required field(s) missing' } )
+                    }
+                    res.json({ assigned_to, status_text, open: true, _id: issue.id, issue_title, issue_text, created_by, created_on: issue.created_on, updated_on: issue.updated_on })
+                }
+                finally {
+                    mongoose.connection.close()
+                }
 
-            const { _id } = req.body
-            const doc = await Issue.findById(_id).catch(() => null)
+            })
 
-            if (!doc) {
-                mongoose.connection.close()
-                return res.json({ error: 'could not update', _id })
-            }
+            .put(async function (req, res) {
+                try {
 
-            for (const prop in req.body) {
-                doc[prop] = req.body[prop] || doc[prop]
-            }
-            if (req.body.open === false) doc.open = false
-            await doc.save()
+                    await mongoose.connect(process.env.MONGO_URI)
+                    let project = req.params.project;
 
-            res.json({ result: 'succesfully updated', _id })
-        })
+                    const { _id } = req.body
+                    if (!_id) return res.status(200).json({ error: 'missing _id' })
 
-        .delete(async function (req, res) {
-            await mongoose.connect(process.env.MONGO_URI)
-            let project = req.params.project;
-            const { _id } = req.body
+                    const fields = ['issue_title', 'issue_text', 'created_by', 'assigned_to', 'status_text']
 
-            const result = await Issue.findByIdAndDelete(_id).catch(() => null)
-            
-            if (result) {
-                res.json({result: "successfully deleted", _id})
-            }
-            else {
-                res.json({error: "could not delete", _id})
-            }
-        });
-    mongoose.connection.close()
+                    if (fields.every(item => !(item in req.body))) {
+                        return res.status(200).json({ error: 'no update field(s) sent', '_id': _id })
+                    }
+
+                    const doc = await Issue.findById(_id).catch(() => null)
+
+                    if (!doc) {
+                        mongoose.connection.close()
+                        return res.status(200).json({ error: 'could not update', _id })
+                    }
+
+                    for (const prop in req.body) {
+                        doc[prop] = req.body[prop] || doc[prop] || ''
+                    }
+                    if (req.body.open === false) doc.open = false
+                    doc.updated_on = new Date().toISOString()
+                    await doc.save()
+
+                    res.json({ result: 'successfully updated', _id })
+                }
+                finally {
+                    mongoose.connection.close()
+                }
+            })
+
+            .delete(async function (req, res) {
+                try {
+                    await mongoose.connect(process.env.MONGO_URI)
+                    let project = req.params.project;
+                    const { _id } = req.body
+                    if (!_id) return res.status(200).json({ error: 'missing _id' })
+
+                    const result = await Issue.findByIdAndDelete(_id).catch(() => null)
+
+                    if (result) {
+                        res.json({ result: "successfully deleted", _id })
+                    }
+                    else {
+                        res.status(200).json({ error: "could not delete", _id })
+                    }
+                }
+                finally {
+                    mongoose.connection.close()
+                }
+            });
+    }
+    catch (e) {
+        console.log(e.message)
+        res.status(500).json({ error: "Something went wrong" })
+    }
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,3 +1,4 @@
+
 const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
@@ -5,36 +6,239 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-const Browser = require('zombie')
-Browser.site = "http://localhost:5000";
+suite('Functional Tests', function () {
+    let _id
+    suite("POST requests", function () {
+        test('All fields', function (done) {
+            chai
+                .request(server)
+                .post('/api/issues/apitest')
+                .send({
+                    'issue_title': 'Issue 1',
+                    'issue_text': '1st test',
+                    'created_by': 'John Doe',
+                    'assigned_to': 'Jonathan Doe',
+                    'status_text': 'Status Msg'
+                })
+                .end((err, res) => {
+                    _id = res.body._id
+                    assert.equal(res.status, 200)
+                    assert.isObject(res.body)
+                    assert.property(res.body, 'created_on')
+                    assert.property(res.body, 'issue_title')
+                    assert.property(res.body, 'issue_text')
+                    assert.property(res.body, 'created_by')
+                    assert.property(res.body, 'status_text')
+                    assert.property(res.body, 'assigned_to')
+                    assert.property(res.body, '_id')
+                    assert.property(res.body, 'updated_on')
+                    done()
+                })
+        })
 
-suite('Functional Tests with Zombie.js', function () {
-    this.timeout(5000);
-    const browser = new Browser();
+        test('Only required fields', function (done) {
+            chai
+                .request(server)
+                .post('/api/issues/apitest')
+                .send({
+                    'issue_title': 'Issue 2',
+                    'issue_text': '2nd test',
+                    'created_by': 'Johnny Doe'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.isObject(res.body)
+                    assert.property(res.body, 'created_on')
+                    assert.property(res.body, 'issue_title')
+                    assert.property(res.body, 'issue_text')
+                    assert.property(res.body, 'created_by')
+                    assert.property(res.body, 'status_text')
+                    assert.property(res.body, 'assigned_to')
+                    assert.property(res.body, '_id')
+                    assert.property(res.body, 'updated_on')
+                    done()
+                })
+        })
 
-    suiteSetup(function (done) {
-        return browser.visit('/', done);
-    });
-
-    suite('Headless browser', function () {
-        test('should have a working "site" property', function () {
-            assert.isNotNull(browser.site);
-        });
-    });
-
-    suite('Form Input', function () {
-        
-        test('Every field', async () => {
-            await browser.fill('issue_title', 'test')
-            await browser.fill('issue_text', 'testing testing 123')
-            await browser.fill('created_by', 'John Doe')
-            await browser.fill('assigned_to', 'John Doe')
-            await browser.fill('status_text', 'statusMsg')
-            const res = await browser.pressButton('Submit Issue')
-            browser.assert.success()
-            browser.assert.elements('code#jsonResult', 1)
-            browser.assert.text('code#jsonResult', "DKfjsdklfjlksd")
-            
+        test('Missing required fields', function (done) {
+            chai
+                .request(server)
+                .post('/api/issues/apitest')
+                .send({
+                    'issue_title': 'Issue 2',
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.isObject(res.body)
+                    assert.property(res.body, 'error')
+                    assert.equal(res.body.error, 'required field(s) missing')
+                    done()
+                })
         })
     })
-})
+
+    suite('GET requests', function () {
+        test('View issues on a project', function (done) {
+            chai
+                .request(server)
+                .get('/api/issues/apitest')
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.isArray(res.body)
+                    done()
+                })
+        })
+
+        test('View issues on a project with 1 filter', function (done) {
+            chai
+                .request(server)
+                .get('/api/issues/apitest?created_by=JohnDoe')
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.isArray(res.body)
+                    done()
+                })
+        })
+
+        test('View issues on a project with multiple filters', function (done) {
+            chai
+                .request(server)
+                .get('/api/issues/apitest?created_by=JohnDoe&assigned_to=Joe')
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.isArray(res.body)
+                    done()
+                })
+        })
+    })
+
+    suite('PUT requests', function () {
+        test('Update one field on an issue', function (done) {
+            chai
+                .request(server)
+                .put('/api/issues/apitest')
+                .send({
+                    '_id': '62780af3958d4721de973044',
+                    'issue_text': 'update test',
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.isObject(res.body)
+                    assert.property(res.body, 'result')
+                    assert.property(res.body, '_id')
+                    done()
+                })
+        })
+        test('Update multiple fields on an issue', function (done) {
+            chai
+                .request(server)
+                .put('/api/issues/apitest')
+                .send({
+                    "_id": '62780af3958d4721de973044',
+                    'issue_title': 'Issue 2',
+                    'issue_text': '2nd test',
+                    'created_by': 'Johnny Doe'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.isObject(res.body)
+                    assert.property(res.body, 'result')
+                    assert.property(res.body, '_id')
+                    done()
+                })
+        })
+        test('Update with missing _id', function (done) {
+            chai
+                .request(server)
+                .put('/api/issues/apitest')
+                .send({
+                    'issue_title': 'Issue 2',
+                    'issue_text': '2nd test',
+                    'created_by': 'Johnny Doe'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.isObject(res.body)
+                    assert.property(res.body, 'error')
+                    done()
+                })
+        })
+
+        test('no fields to update', function (done) {
+            chai
+                .request(server)
+                .put('/api/issues/apitest')
+                .send({
+                    "_id": '62780af3958d4721de973044',
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.isObject(res.body)
+                    assert.property(res.body, 'error')
+                    assert.equal(res.body.error, 'no update field(s) sent')
+                    done()
+                })
+        })
+        test('invalid _id', function (done) {
+            chai
+                .request(server)
+                .put('/api/issues/apitest')
+                .send({
+                    "_id": 'ddd',
+                    'issue_title': 'Issue 2',
+                    'issue_text': '2nd test',
+                    'created_by': 'Johnny Doe'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.isObject(res.body)
+                    assert.property(res.body, 'error')
+                    done()
+                })
+        })
+    })
+    suite('DELETE requests', function () {
+        test('valid delete request', function (done) {
+            chai
+                .request(server)
+                .delete('/api/issues/apitest')
+                .send({
+                    _id
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.property(res.body, 'result')
+                    assert.property(res.body, '_id')
+                    done()
+                })
+        })
+        test('invalid _id', function (done) {
+            chai
+                .request(server)
+                .delete('/api/issues/apitest')
+                .send({
+                    '_id': 'dsddf'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.property(res.body, 'error')
+                    assert.property(res.body, '_id')
+                    done()
+                })
+        })
+        test('missing _id', function (done) {
+            chai
+                .request(server)
+                .delete('/api/issues/apitest')
+                .send({
+                    '_id': ''
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.deepEqual(res.body, { error: 'missing _id' })
+                    done()
+                })
+        })
+
+    })
+});
