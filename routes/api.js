38fe4eb6d87d2a79106d35fd43210569eab0458b
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-05-08 18:55:00 +0200

    finished routes

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,67 @@
 'use strict';
+const Issue = require('../model')
+const mongoose = require('mongoose')
 
 module.exports = function (app) {
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
-    });
-    
+    app.route('/api/issues/:project')
+        .get(async function (req, res) {
+            await mongoose.connect(process.env.MONGO_URI)
+            const fields = ['_id', 'issue_title', 'issue_text', 'created_by', 'assigned_to', 'status_text', 'created_on', 'updated_on', 'open' ]
+            let project = req.params.project;
+            let query = {}
+            for (const prop in req.query) {
+                if (fields.includes(prop.toLowerCase())) query[prop] = req.query[prop]
+            }
+            const result = await Issue.find(query)
+            res.json([...result])
+        })
+
+        .post(async function (req, res) {
+            await mongoose.connect(process.env.MONGO_URI)
+            let project = req.params.project;
+            const { issue_title, issue_text, created_by, assigned_to, status_text } = req.body
+            const issue = new Issue({ ...req.body, open: true, created_on: new Date().toISOString(), updated_on: new Date().toISOString() })
+            await issue.save()
+
+            res.json({ assigned_to, status_text, open: true, _id: issue.id, issue_title, issue_text, created_by, created_on: issue.created_on, updated_on: issue.updated_on })
+
+        })
+
+        .put(async function (req, res) {
+            await mongoose.connect(process.env.MONGO_URI)
+            let project = req.params.project;
+
+            const { _id } = req.body
+            const doc = await Issue.findById(_id).catch(() => null)
+
+            if (!doc) {
+                mongoose.connection.close()
+                return res.json({ error: 'could not update', _id })
+            }
+
+            for (const prop in req.body) {
+                doc[prop] = req.body[prop] || doc[prop]
+            }
+            if (req.body.open === false) doc.open = false
+            await doc.save()
+
+            res.json({ result: 'succesfully updated', _id })
+        })
+
+        .delete(async function (req, res) {
+            await mongoose.connect(process.env.MONGO_URI)
+            let project = req.params.project;
+            const { _id } = req.body
+
+            const result = await Issue.findByIdAndDelete(_id).catch(() => null)
+            
+            if (result) {
+                res.json({result: "successfully deleted", _id})
+            }
+            else {
+                res.json({error: "could not delete", _id})
+            }
+        });
+    mongoose.connection.close()
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,36 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
-});
+const Browser = require('zombie')
+Browser.site = "http://localhost:5000";
+
+suite('Functional Tests with Zombie.js', function () {
+    this.timeout(5000);
+    const browser = new Browser();
+
+    suiteSetup(function (done) {
+        return browser.visit('/', done);
+    });
+
+    suite('Headless browser', function () {
+        test('should have a working "site" property', function () {
+            assert.isNotNull(browser.site);
+        });
+    });
+
+    suite('Form Input', function () {
+        
+        test('Every field', async () => {
+            await browser.fill('issue_title', 'test')
+            await browser.fill('issue_text', 'testing testing 123')
+            await browser.fill('created_by', 'John Doe')
+            await browser.fill('assigned_to', 'John Doe')
+            await browser.fill('status_text', 'statusMsg')
+            const res = await browser.pressButton('Submit Issue')
+            browser.assert.success()
+            browser.assert.elements('code#jsonResult', 1)
+            browser.assert.text('code#jsonResult', "DKfjsdklfjlksd")
+            
+        })
+    })
+})
